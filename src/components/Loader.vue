d938bcd123f04deff48f904d66189c6c86e05ccb
Paweł <pawel.brachaczek@enginiety.com>
2019-11-20 09:45:58 +0100

    new endpoint, loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="a-loader">
+      <div class="lds-ripple"><div></div><div></div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component} from 'vue-property-decorator';
+
+
+@Component({
+    components: {
+    }
+})
+export default class Loader extends Vue {
+}
+</script>
